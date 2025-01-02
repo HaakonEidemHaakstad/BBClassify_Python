3a6de34f9862aee72c75c7c062a9808a5811d2ae
Haakon Eidem Haakstad <h.e.haakstad@gmail.com>
2025-01-02 14:13:56 +0100

    ui input validation

--- ui.py
+++ ui.py
@@ -23,7 +23,7 @@ def read_input(filename: str) -> list:
 
     ## Line 1
     if len(lines[0]) not in [3, 4]:
-        raise ImportError("The first line of the input file must have at least 3 and at most 4 entries.")
+        raise ValueError("The first line of the input file must have at least 3 and at most 4 entries.")
     if lines[0[0].lower()] not in ["hb", "ll"]:
         raise ValueError(f"The first value in the first row of the input file ({filename}) must be either 'hb' (specifying the Hanson and Brennan procedure) or 'll' (specifying the Livingston and Lewis procedure).")
     if not isinstance(lines[0][1], (float, int)):
@@ -35,13 +35,13 @@ def read_input(filename: str) -> list:
     
     ## Line 2
     if len(lines[1]) not in [2, 4, 5]:
-        raise ImportError(f"The second row of the input file ({filename}) must contain either 2 (if raw-score moments are supplied), 4 (if a raw-score- or frequency distribution is supplied). If the Livingston and Lewis procedure is specified in line 1, an optional fifth value can be specified to indicate the minimum possible score to attain on the test (default is 0).")
+        raise ValueError(f"The second row of the input file ({filename}) must contain either 2 (if raw-score moments are supplied), 4 (if a raw-score- or frequency distribution is supplied). If the Livingston and Lewis procedure is specified in line 1, an optional fifth value can be specified to indicate the minimum possible score to attain on the test (default is 0).")
     if not isinstance(lines[1][1], str) or lines[1][1] not in ["R", "r", "F", "f", "M", "m"]:
         raise ValueError(f"The second value in the second row of the input file ({filename}) must be either R or r (indicating that the contents of the data-file ({lines[1][0]} represent final test-scores), F or f (specifying that the contents of {lines[1][1]} represents a frequency distribution, or M or m (specifying that the {lines[1][1]} contains raw-score moments).)")
     if lines[1][1] in ["M", "m"] and len(lines[1]) != 2:
-        raise ValueError(f"If moment input is specified, the data-file ({filename}) and the input type must be specified in the second ")
+        raise ValueError(f"If moment input is specified, the data-file ({lines[1][0]}) and the input type must be specified in the second ")
     if lines[1][1] not in ["M", "m"] and (not isinstance(lines[1][2], (float, int)) or lines[1][2] % 1 != 0):
-        raise TypeError(f"If the second value in the second row of the input file ({filename}) is not M or m, the column of the data-file ({lines[1][1]}) that contains the test-scores must be specified as the third value in the second row of the input file ({filename}).") 
+        raise TypeError(f"If the second value in the second row of the input file ({filename}) is not M or m, the column of the data-file ({lines[1][0]}) that contains the test-scores must be specified as the third value in the second row of the input file ({filename}).") 
     if lines[1][1] in ["R", "r", "F", "f"] and (not isinstance(lines[1][3], (float, int)) or lines[1][3] % 1 != 0):
         raise TypeError(f"If the second value in the second row of the input file ({filename} is R or r, or F or f, the fourth value of the second row of the input file must be an integer specifying either (if a raw-score distribution is specified) the number of test items (for the Hanson and Brennan procedure) or the maximum possible test-score (for the Livingston and Lewis procedure), or (if a frequency distribution is specified) the column in the data-file ({filename}) containing the frequencies of sum-score occurances (if F or f).)")
     if not isinstance(lines[1][2], (float, int)) or lines[1][2] % 1 != 0:
@@ -56,7 +56,7 @@ def read_input(filename: str) -> list:
         raise TypeError(f"The first value of the third row of the input file ({filename}) must be an integer.")
     if not isinstance(lines[2][0], (float, int)) or lines[2][1] % 1 != 0:
         raise TypeError(f"The second value of the third row of the input file ({filename}) must be an integer.")
-    if not isinstance(lines[2, 2], float) or 0 >= lines[2][2] >= 1:
+    if len(lines[2]) == 3 and (not isinstance(lines[2, 2], float) or 0 >= lines[2][2] >= 1):
         raise TypeError(f"The third value of the third row of the input file ({filename}) must be a value between 0 and 1.")
     
     return lines[0:3]
