ecde66c68f7ace44ffe7715e34668f82a21e1456
Haakon Eidem Haakstad <h.e.haakstad@gmail.com>
2025-01-19 00:03:38 +0100

    ui code cleaning, type hints, input validation

--- ui.py
+++ ui.py
@@ -1,21 +1,21 @@
 import os
 import sys
 import numpy as np
-import pandas as pd
 import bbclassify
 import statistics as stats
 import scipy.stats
+
 def read_and_parse_input(filename: str) -> list:
-    input_error = "Input error. Execution terminated."
+    input_error: str = "Input error. Execution terminated."
     #if not os.path.isabs(filename):
     #    filename = os.path.join(os.path.abspath(__file__), filename)
     #try:
-    with open (filename, "r") as file:
-           lines = file.readlines()
+    with open(filename, "r") as file:
+           lines: list = file.readlines()
     #except:
     #    raise ImportError("Error reading input file. Check whether the file-path is correctly specified.")
-    lines = [i.lower().split() for i in lines]
-    lines = [[float(i) if i.replace(".", "", 1).replace("-", "", 1).isdigit() else i for i in j] for j in lines]
+    lines: list = [i.lower().split() for i in lines]
+    lines: list = [[float(i) if i.replace(".", "", 1).replace("-", "", 1).isdigit() else i for i in j] for j in lines]
 
     # Input validation.
         # Line 1.
@@ -75,12 +75,12 @@ def read_and_parse_input(filename: str) -> list:
             raise TypeError(input_error)
     
         # Line 3.
-    #if not isinstance(lines[2][0], (float, int)):
-    #    print(f"All entries in the third line of the input file must be numeric. Current input is {[lines[2]]}.")
-    #    raise TypeError(input_error)
-    #if len(lines[2]) == 3 and (not all(isinstance(i, float) for i in lines[2][2]) or not all(0 > i > 1 for i in lines[2][2])):
-    #    print(f"All true-score cut-points must be floating point values between 0 and 1 (0 < cut-point < 1). Current input is {lines[2][2]}.")
-    #    raise TypeError(input_error)
+    if not isinstance(lines[2][0], (float, int)):
+        print(f"All entries in the third line of the input file must be numeric. Current input is {[lines[2]]}.")
+        raise TypeError(input_error)
+    if len(lines[2]) == 3 and (not all(isinstance(i, float) for i in lines[2][2]) or not all(0 > i > 1 for i in lines[2][2])):
+        print(f"All true-score cut-points must be floating point values between 0 and 1 (0 < cut-point < 1). Current input is {lines[2][2]}.")
+        raise TypeError(input_error)
     if len(lines[2]) == lines[2][0]:
         lines[2] = [lines[2][0], lines[2][1:]]
     elif len(lines[2]) == lines[2][0]*2 - 1:
@@ -93,37 +93,37 @@ def read_and_parse_input(filename: str) -> list:
     return lines
 
 
-def read_and_parse_data(parsed_input: str) -> list:
+def read_and_parse_data(parsed_input: list) -> tuple:
     datafile: str = parsed_input[1][0].removeprefix('"').removesuffix('"')
     if not os.path.isabs(datafile):
         datafile = os.path.join(os.path.abspath(__file__), datafile)
     datafile = datafile.replace("/ui.py", "")
     with open (datafile, 'r') as file:
-        datalines = file.readlines()
+        datalines: list = file.readlines()
     if parsed_input[1][1].lower() == "r":
-        data = [float(i[0]) if float(i[0] % 1 != 0) else int(i[0]) for i in datalines]
+        data: list = [float(i[0]) if float(i[0] % 1 != 0) else int(i[0]) for i in datalines]
         datalines = None
     elif parsed_input[1][1].lower() == "f":
         datalines = [[float(k) if float(k) % 1 != 0 else int(k) for k in "".join(i if i.isdigit() or i == "." else " " for i in j).split(" ")[:2]] for j in datalines]
-        xcol = int(parsed_input[1][2] - 1)
-        fcol = int(parsed_input[1][3] - 1)
-        data_full = [[i[xcol] for _ in range(i[fcol])] for i in datalines]
+        xcol: int = int(parsed_input[1][2] - 1)
+        fcol: int = int(parsed_input[1][3] - 1)
+        data_full: list[list] = [[i[xcol] for _ in range(i[fcol])] for i in datalines]
         data = [i for j in data_full for i in j]   
-    #non_numeric = [i for i in data[0] if not isinstance(i, (float, int))]
-    #len_non_numeric = len(non_numeric)
-    #if len_non_numeric != 0:
-    #    print(f"All values contained in the data-file must be numeric. Data-file contained {len_non_numeric} non-numeric {"entries" if len_non_numeric > 1 else "entry"}.")
-    #    print(f"Ensure that the data-file contains no non-numeric entries (e.g., no column-names).")
-    #    if len_non_numeric < 7:
-    #        print(f"The data-file contained the following non-numeric {"entries" if len_non_numeric > 1 else "entry"}: {non_numeric}.")
-    #    else:
-    #        print(f"The first six non-numeric entries encountered in the data-file were: {non_numeric[:6]}.")
-    #    raise TypeError("Input error. Execution terminated.")
+    non_numeric = [i for i in data if not isinstance(i, (float, int))]
+    len_non_numeric = len(non_numeric)
+    if len_non_numeric != 0:
+        print(f"All values contained in the data-file must be numeric. Data-file contained {len_non_numeric} non-numeric {"entries" if len_non_numeric > 1 else "entry"}.")
+        print(f"Ensure that the data-file contains no non-numeric entries (e.g., no column-names).")
+        if len_non_numeric < 7:
+            print(f"The data-file contained the following non-numeric {"entries" if len_non_numeric > 1 else "entry"}: {non_numeric}.")
+        else:
+            print(f"The first six non-numeric entries encountered in the data-file were: {non_numeric[:6]}.")
+        raise TypeError("Input error. Execution terminated.")
     return data, datalines
 
 
 def float_to_str(x: float) -> str:
-    x = str(round(x, 5))
+    x: str = str(round(x, 5))
     if "." not in x and len(x) < 7:
         x = x + "." + "0"*(7 - len(x) -1)
     elif "." in x and len(x) < 7:
@@ -132,54 +132,58 @@ def float_to_str(x: float) -> str:
         x = x[:7]
     elif "." in x and x.index(".") >= 7:
         x = x[:x.index(".")]
-    return x
-    
+    return x    
 
-def array_to_strlist(x: np.array):
-    x = np.hstack((x, x.sum(axis = 1).reshape(-1, 1)))
-    x = np.vstack((x, x.sum(axis = 0)))
-    strlist = [[round(float(i), 5) if i > 0.0001 else 0 for i in x[j, ]] for j in range(len(x))]
+def array_to_strlist(x: np.array) -> list[str]:
+    x: np.array = np.hstack((x, x.sum(axis = 1).reshape(-1, 1)))
+    x: np.array = np.vstack((x, x.sum(axis = 0)))
+    strlist: list = [[round(float(i), 5) if i > 0.0001 else 0 for i in x[j, ]] for j in range(len(x))]
     for i in range(len(x)):
         for j in range(len(x)):
             strlist[i][j] = float_to_str(strlist[i][j])
     return strlist
 
-def add_labels(x, col, row):
-    col = [""] + [col + str(i) if i < len(x) - 1 else "marg" for i in range(len(x))]
-    col = [" "*(7 - len(i)) + i for i in col]
-    row = [row + str(i) if i < len(x) - 1 else "marg" for i in range(len(x))]
-    row = [" "*(7 - len(i)) + i for i in row]
-    x = [[row[i]] + x[i] for i in range(len(row))]
+def add_labels(x: list[list], col:  int, row: int) -> list:
+    col: list = [""] + [col + str(i) if i < len(x) - 1 else "marg" for i in range(len(x))]
+    col: list = [" "*(7 - len(i)) + i for i in col]
+    row: list = [row + str(i) if i < len(x) - 1 else "marg" for i in range(len(x))]
+    row: list = [" "*(7 - len(i)) + i for i in row]
+    x: list[list] = [[row[i]] + x[i] for i in range(len(row))]
     x.insert(0, col)
     return x
 
 def main():
     input_file: str = input("Enter path to- or name of the input file: ")
-    input_file_name = input_file[::-1].split("/")[0][::-1]
+    input_file_name: str = input_file[::-1].split("/")[0][::-1]
     input_file: list = read_and_parse_input(input_file)
 
-    data = read_and_parse_data(input_file)
-    n_observations = len(data[0])
-    n_categories = int(input_file[2][0])
-    mean = stats.mean(data[0])
-    variance = stats.variance(data[0])
-    skewness = scipy.stats.skew(data[0])
-    kurtosis = scipy.stats.kurtosis(data[0], fisher = False)
-    reliability = input_file[0][1]
+    method: str = input_file[0][0]
+    model: int = input_file[0][2]
+    minimum_expected_value: float = input_file[0][3]
+
+    data: list = read_and_parse_data(input_file)
+
+    n_observations: int = len(data[0])
+    n_categories: int = int(input_file[2][0])
+
+    mean: float = stats.mean(data[0])
+    variance: float = stats.variance(data[0])
+    skewness: float = scipy.stats.skew(data[0])
+    kurtosis: float = scipy.stats.kurtosis(data[0], fisher = False)
+
+    reliability: float = input_file[0][1]
+
     if input_file[1][1].lower() == "r":
-        min_score = input_file[1][4] if input_file[0][0].lower() == "ll" else 0
-        max_score = input_file[1][3]
+        min_score: float = input_file[1][4] if input_file[0][0].lower() == "ll" else 0
+        max_score: float = input_file[1][3]
     if input_file[1][1].lower() == "f":
-        min_score = data[1][0][0]
-        max_score = data[1][len(data[1]) - 1][0]
+        min_score: float = data[1][0][0]
+        max_score: float = data[1][len(data[1]) - 1][0]
     cut_scores = [float(i) if float(i) % 1 != 0 else int(i) for i in input_file[2][1]]
     if len(input_file[2]) == 3:
-        cut_truescores = input_file[2][2]
+        cut_truescores: list = input_file[2][2]
     else:
-        cut_truescores = None
-    method = input_file[0][0]
-    model = input_file[0][2]
-    minimum_expected_value = input_file[0][3]
+        cut_truescores = None    
     
     print("PROGRESS:")
     print(" Estimating model parameters...", end = "\r")
@@ -191,20 +195,23 @@ def main():
                                     cut_truescores = cut_truescores,
                                     method = method,
                                     model = model, 
-                                    failsafe = True,
-                                    l = 0.223172)
-    ts_raw_moments = output._tsm(data[0], output.max_score if method.lower() != "ll" else output.effective_test_length, output.Parameters["lords k"])
-    ts_moments = [ts_raw_moments[0], ts_raw_moments[1] - ts_raw_moments[0]**2]
+                                    failsafe = True)    
+    print(" Estimating model parameters... \033[92m✓\033[0m")
+    
+    ts_raw_moments: list = output._tsm(data[0], output.max_score if method.lower() != "ll" else output.effective_test_length, output.Parameters["lords k"])
+    ts_moments: list = [ts_raw_moments[0], ts_raw_moments[1] - ts_raw_moments[0]**2]
     ts_moments.append((ts_raw_moments[2] - 3*(ts_raw_moments[0]*ts_raw_moments[1]) + 2*ts_raw_moments[0]**3) / (ts_moments[1]**.5)**3)
     ts_moments.append((ts_raw_moments[3] - 4*(ts_raw_moments[0] * ts_raw_moments[2]) + 6*(ts_raw_moments[0]**2 * ts_raw_moments[1]) - 3*ts_raw_moments[0]**4) / (ts_moments[1]**.5)**4)
-    print(" Estimating model parameters... \033[92m✓\033[0m")
+
     print(" Estimating model fit...", end = "\r")
     output.modelfit(minimum_expected_value = minimum_expected_value)
     print(" Estimating model fit... \033[92m✓\033[0m")
+
     print(" Estimating classification accuracy...", end = "\r")
     output.accuracy()
-    print(" Estimating classification accuracy... \033[92m✓\033[0m")    
-    rounded_confusionmatrix = add_labels(array_to_strlist(output.confusionmatrix.transpose()), "x", "t")
+    print(" Estimating classification accuracy... \033[92m✓\033[0m")
+
+    rounded_confusionmatrix: list[list] = add_labels(array_to_strlist(output.confusionmatrix.transpose()), "x", "t")
     tp, tn, fp, fn, sensitivity, specificity = [], [], [], [], [], []
     for i in range(n_categories):
         tp.append(output.confusionmatrix[i, i])
@@ -213,7 +220,19 @@ def main():
         tn.append(1 - (tp[i] + fp[i] + fn[i]))
         sensitivity.append(tp[i] / (tp[i] + fn[i]))
         specificity.append(tn[i] / (tn[i] + fp[i]))
-    category_proportions = []
+
+    print(" Estimating classification consistency... ", end = "\r")
+    output.consistency()
+    print(" Estimating classification consistency... \033[92m✓\033[0m")
+
+    rounded_consistencymatrix: list[list] = add_labels(array_to_strlist(output.consistencymatrix), "x", "x")    
+    chance_consistency: list = [sum(i)**2 for i in output.consistencymatrix]
+    coefficient_kappa: float = (output.Consistency - sum(chance_consistency)) / (1 - sum(chance_consistency))
+
+    weighted_consistencymatrix: list[list] = [i / sum(i) for i in output.consistencymatrix]
+    coefficient_kappas: list = [(output.consistencymatrix[i][i] - chance_consistency[i]) / (1 - chance_consistency[i]) for i in range(n_categories)]
+
+    category_proportions: list = []
     for i in range(n_categories):
         if i == 0:
             category_proportions.append(len([i for i in data[0] if i < input_file[2][1][0]]) / n_observations)
@@ -221,13 +240,7 @@ def main():
             category_proportions.append(len([j for j in data[0] if j >= cut_scores[i - 1] and j < cut_scores[i]]) / n_observations)
         else:
             category_proportions.append(len([i for i in data[0] if i >= cut_scores[len(cut_scores) - 1]]) / n_observations)
-    print(" Estimating classification consistency... ", end = "\r")
-    output.consistency()
-    print(" Estimating classification consistency... \033[92m✓\033[0m")
-    rounded_consistencymatrix = add_labels(array_to_strlist(output.consistencymatrix), "x", "x")
-    chance_consistency = sum([sum(i)**2 for i in output.consistencymatrix])
-    coefficient_kappa = (output.Consistency - chance_consistency) / (1 - chance_consistency)
-
+    
     with open("BBClassify_output", "w") as file:
 
         file.write("******************************************************************************\n")
@@ -242,12 +255,12 @@ def main():
         file.write("\n")
         file.write(f"*** Listing of Input Specified in \"{input_file_name}\" ***\n")
         file.write("\n")
-        file.write(f" Type of Procedure:           {"Livingston and Lewis ('LL')." if method.lower() == "ll" else "Hanson and Brennan ('HB').\n"}")
+        file.write(f" Type of Procedure:           {"Livingston and Lewis ('LL')." if method.lower() == "ll" else "Hanson and Brennan ('HB')\n"}")
         file.write(f" Reliability of scores:       {reliability}\n")
-        file.write(f" True-score Beta model:       {int(model)}-parameter Beta distribution.\n")
-        file.write(f" Model-fit testing:           Minimum expected value of bins set to {minimum_expected_value}.\n")
+        file.write(f" True-score Beta model:       {int(model)}-parameter Beta distribution\n")
+        file.write(f" Model-fit testing:           Minimum expected value of bins set to {minimum_expected_value}\n")
         file.write(f" Name of data file:           {input_file[1][0]}\n")
-        file.write(f" Format of input data:        {"Raw scores" if input_file[1][1].lower() == "r" else "Frequency distribution of raw scores"}.\n")
+        file.write(f" Format of input data:        {"Raw scores" if input_file[1][1].lower() == "r" else "Frequency distribution of raw scores"}\n")
         file.write(f" Maximum possible score:      {max_score}\n")
         file.write(f" Minimum possible score:      {min_score}\n")
         file.write(f" Number of categories:        {int(input_file[2][0])}\n")
@@ -309,16 +322,17 @@ def main():
         file.write(" Confusion matrix:\n")
         for i in range(len(rounded_confusionmatrix)): file.write(f"{"   ".join(rounded_confusionmatrix[i])}\n")
         file.write("\n")
-        file.write(f" Overall:\n")
+        file.write(f" Overall:                  Unweighted  Weighted\n")
         file.write(f"  Accuracy:                   {float_to_str(output.Accuracy)}\n")
-        file.write(f"  Sensitivity:                {float_to_str(sum([sensitivity[i] * category_proportions[i] for i in range(n_categories)]))} (weighted average)\n")
-        file.write(f"  Specificity:                {float_to_str(sum([specificity[i] * category_proportions[i] for i in range(n_categories)]))} (weighted average)\n")
+        file.write(f"  Sensitivity:                {float_to_str(np.mean(sensitivity))}   {float_to_str(sum([sensitivity[i] * category_proportions[i] for i in range(n_categories)]))}\n")
+        file.write(f"  Specificity:                {float_to_str(np.mean(specificity))}   {float_to_str(sum([specificity[i] * category_proportions[i] for i in range(n_categories)]))}\n")
         file.write("\n")
         file.write(" Category specific:\n")
         file.write("\n")
         for i in range(n_categories):
             file.write(f"  Category {i + 1}:\n")
-            file.write(f"   Accuracy:                  {float_to_str(tp[i])}\n")
+            file.write(f"   True Positives:            {float_to_str(tp[i])}\n")
+            file.write(f"   True Negatives:            {float_to_str(tn[i])}\n")
             file.write(f"   Sensitivity:               {float_to_str(sensitivity[i])}\n")
             file.write(f"   Specificity:               {float_to_str(specificity[i])}\n")
             file.write("\n")
@@ -328,17 +342,17 @@ def main():
         file.write(" Consistency matrix:\n")
         for i in range(len(rounded_consistencymatrix)): file.write(f"{"   ".join(rounded_consistencymatrix[i])}\n")
         file.write("\n")
-        file.write(f" Overall:\n") 
+        file.write(f" Overall:                  Unweighted  Weighted\n") 
         file.write(f"  Consistency:                {float_to_str(output.Consistency)}\n")
-        file.write(f"  Chance consistency:         {float_to_str(chance_consistency)}\n")
+        file.write(f"  Chance consistency:         {float_to_str(sum(chance_consistency))}\n")
         file.write(f"  Coefficient Kappa:          {float_to_str(coefficient_kappa)}\n")
         file.write("\n")
         file.write(" Category specific:\n")
         for i in range(n_categories):
             file.write(f"  Category {i + 1}:\n")
-            file.write(f"   Consistency:               {float_to_str(output.consistencymatrix[i, i])}\n")
+            file.write(f"   Consistency:               {float_to_str(output.consistencymatrix[i, i])}   {float_to_str(weighted_consistencymatrix[i][i])}\n")
             file.write(f"   Chance consistency:        {float_to_str(sum(output.consistencymatrix[i, :])**2)}\n")
-            file.write(f"   Coefficient Kappa:         {float_to_str((output.consistencymatrix[i, i] - sum(output.consistencymatrix[i, :])**2) / (1 - sum(output.consistencymatrix[i, :])**2))}\n")
+            file.write(f"   Coefficient Kappa:         {float_to_str(coefficient_kappas[i])}\n")
             file.write("\n")
 
 if __name__ == "__main__":
