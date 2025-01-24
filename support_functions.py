ff60c260041c4907cdc757167ca0ac51318f646e
= <=>
2025-01-24 15:57:22 +0100

    t

--- support_functions.py
+++ support_functions.py
@@ -101,13 +101,12 @@ def read_and_parse_input(filename: str, raw = False) -> list:
 def read_and_parse_data(parsed_input: list) -> tuple:
     datafile: str = parsed_input[1][0].removeprefix('"').removesuffix('"')
     system_name: str = platform.system()
-    if ".py" in datafile:
-        if system_name == "Windows":
-            re.sub(r"[^\\]*\.py\\", r"\\", datafile)
-        else:
-            re.sub(r"/[^/]*\.py/", "/", datafile)
     if not os.path.isabs(datafile):
-        datafile = os.path.join(os.path.abspath(__file__), datafile)
+        datafile = os.path.join(os.path.abspath(__file__), datafile)        
+    filename = filename.replace("support_functions.py/", "")
+    filename = filename.replace("support_functions.py\\", "")
+    filename = filename.replace("ui.py/", "")
+    filename = filename.replace("ui.py\\", "")
     with open (datafile, 'r') as file:
         datalines: list = file.readlines()
     if parsed_input[1][1].lower() == "r":
